49456846a822e1f90e55cefc023ec503a5fd0c88
locross <locross15@gmail.com>
2022-04-29 13:21:34 +1000

    added style buttons

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 function about(){
     alert('Link to about page')
-}
+
+};
+
 function reset(){
     formFH.fNameIn.value = '';
     formFH.lNameIn.value = '';
     formFH.eMailIn.value = '';
     formFH.phoneIn.value = '';
-    formFH.cityIn.value = '';
+    document.getElementById('cityIn').value = '';
     formSH.experience1In.value = '';
     formSH.experience1Info.value = '';
     formSH.experience2In.value = '';
     formSH.experience2Info.value = '';
     formSH.experience3In.value = '';
     formSH.experience3Info.value = '';
-}
+
+};
 
 function saveValues(){
     const fname = formFH.fNameIn.value;
@@ -38,7 +41,8 @@ function saveValues(){
     console.log(experience2Info);
     console.log(experience3);
     console.log(experience3Info);
-}
+
+};
 
 function output(){
     document.getElementById('heading').innerText = formFH.fNameIn.value + ' ' + formFH.lNameIn.value;
@@ -46,10 +50,34 @@ function output(){
     document.getElementById('phoneOut').innerText = formFH.phoneIn.value;
     document.getElementById('cityOut').innerText = formFH.cityIn.value;
     document.getElementById('experience1Out').innerText = formSH.experience1In.value;
-    document.getElementById('experience1InfoOut').innerText = formSH.experience1Info.value;
+    document.getElementById('experience1InfoOut').innerHTML = formSH.experience1Info.value;
     document.getElementById('experience2Out').innerText = formSH.experience2In.value;
     document.getElementById('experience2InfoOut').innerText = formSH.experience2Info.value;
     document.getElementById('experience3Out').innerText = formSH.experience3In.value;
     document.getElementById('experience3InfoOut').innerText = formSH.experience3Info.value;
     
+};
+
+const fontSelections = new Array;
+fontSelections[1] = 'Hind Madurai';
+fontSelections[2] = 'Montserrat';
+fontSelections[3] = 'Roboto Condensed';
+fontSelections[4] = 'Titillium Web';
+
+let fontChanger = (fontSelections) => {
+    document.getElementById('page').style.fontFamily = fontSelections;
+}
+
+let colorChanger = (color) => {
+    document.getElementById('page').style.color = color
+}
+
+const backgroundSelections = new Array;
+backgroundSelections[1] = "url('https://i.pinimg.com/736x/59/6b/8f/596b8fed52b2cedb3addf527c150534c.jpg')";
+backgroundSelections[2] = "url('https://thumbs.dreamstime.com/b/watery-document-background-20184231.jpg')";
+backgroundSelections[3] = "url('https://png.pngtree.com/background/20210710/original/pngtree-light-paper-texture-textured-background-picture-image_1043204.jpg')";
+backgroundSelections[4] = "url('https://i.pinimg.com/736x/30/89/32/3089327d41ec6ede2cf3ea1d31e9488c.jpg')";
+
+let backgroundChanger = (backgroundSelections) => {
+    document.getElementById('page').style.backgroundImage = backgroundSelections;
 }
